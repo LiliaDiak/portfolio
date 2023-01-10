9329325fe8471a04573168d2f20dcaedbae3bb0c
LiliaDiak <lilidiakovychhh@gmail.com>
2023-01-10 15:32:21 +0200

    the end(bad skills in vue(do soft with vue loop) and not perfect burger menu)

--- app/script.js
+++ app/script.js
@@ -1,11 +1,6 @@
-// $(function(){
-//     $('.slider').slick({
-//       arrows:true,
-//       nextArrow:'<button type="button" class="slick-prev"><img src="images/left.svg" alt=""></button>',
-//       prevArrow:'<button type="button" class="slick-next"><img src="images/right.svg" alt=""></button>',
-//       slickPlay: true,
-//     });
-// });
+ $(function(){
+  $("#images").removeAttr("style")
+});
 
 new Vue({
   el: "#app",
@@ -54,3 +49,4 @@ new Vue({
 
   }
 });
+
